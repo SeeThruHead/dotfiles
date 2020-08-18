5b67d5ce44509061536889e68675c17a68d5710b
Shane Keulen <shane.keulen@influitive.com>
2020-08-18 16:29:48 -0400

    encript infl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,5 @@ set -x DISCOURSE_RELATIVE_URL_ROOT "/forum"
 set -x DISCOURSE_DB_USERNAME postgres
 set -x DISCOURSE_DB_PASSWORD mysecretpassword
 set -x PLUGINATOR_REDIS_URL redis://10.0.2.2/9
+set -x GPG_TTY (tty)
 status --is-interactive; and source (rbenv init -|psub)
