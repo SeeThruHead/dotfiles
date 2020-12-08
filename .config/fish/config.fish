7a8b2723fca3b1918b2ffed3a73c2b58a8e6f7d3
Shane Keulen <shane.keulen@influitive.com>
2020-12-08 10:16:11 -0500

    functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ set -x DISCOURSE_DB_USERNAME postgres
 set -x DISCOURSE_DB_PASSWORD mysecretpassword
 set -x PLUGINATOR_REDIS_URL redis://10.0.2.2/9
 set -x GPG_TTY (tty)
+
+set -x AWS_DEFAULT_REGION us-east-1
+set -x AWS_DEFAULT_PROFILE saml
---
+++ .config/fish/functions/loopback.fish
@@ -0,0 +1,3 @@
+function loopback
+    sudo ifconfig lo0 alias 10.0.2.2
+end
